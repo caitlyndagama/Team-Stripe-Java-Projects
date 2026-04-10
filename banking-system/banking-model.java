089f890e106379c76db98e106f72753bd6c64851
Michael Katete <michaelkatete39@gmail.com>
2026-04-10 09:34:27 +0200

    Created Abstract Class

--- banking-system/banking-model.java
+++ banking-system/banking-model.java
@@ -1,1 +1,72 @@
+import java.util.ArrayList;
 
+public abstract class BankingAccount{
+    private String AccountNumber;
+    private String OwnerName;
+    private double balance;
+    
+    public BankingAccount(String AccountNumber, String OwnerName, double initialBalance){
+    this.AccountNumber = AccountNumber;
+    this.OwnerName = OwnerName;
+    this.balance = initialBalance;
+    private ArrayList<String> transactionHistory = new ArrayList<>();}
+
+
+    public String getAccountNumber() {
+        return AccountNumber;
+    }
+
+    public String getOwnerName() {
+        return OwnerName;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public getTransactionHistory() {
+        return transactionHistory;
+    
+    
+    }
+
+    protected void deductFromBalance(double amount){}
+
+    public abstract void withdraw(double amount){
+
+        throw new InsufficientFundsException("The Amount is Insifficient to be Withdraw");
+
+
+    }
+
+    public void deposit(double amount){
+        if(amount > 0){
+            balance += amount;
+            transactionHistory.add("Deposited: " + amount);
+        } 
+        else {
+            throw new IllegalArgumentException("Deposit amount must be positive.");
+        }
+
+    }
+
+    public void printStatement(){
+
+        if(transactionHistory.isEmpty()){
+            System.out.println("No transactions yet.");
+        } 
+        else if (transactionHistory.size() == 5){
+            System.out.println("Transaction History:");
+            for(String transaction : transactionHistory){
+                System.out.println(transaction);
+            }
+        }
+    }
+
+
+
+
+
+
+
+}
